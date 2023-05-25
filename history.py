b45b7c4423063c90aabdc33926cde16e90992cda
Oskar van Velden <oskar@rakso.nl>
2023-05-25 08:39:28 +0200

    Refactor history caching to class

--- history.py
+++ history.py
@@ -1,31 +1,40 @@
 import os
 import pickle
 
-HISTORY_FILE = ".__history.cache"
+HISTORY_FILE_POSTFIX = "__history.cache"
+
+class History:
+
+    _file_name = ''
+
+    def __init__(self, name):
+        self._file_name = f'.{name}{HISTORY_FILE_POSTFIX}'
+
+        if not os.path.isfile(self._file_name):
+            print(f"History::__init__ : Create file {self._file_name}")
+
+            self.write_file()
 
 
-def get_history():
-    with open(HISTORY_FILE, "rb") as fp:
-        return pickle.load(fp)
+    def get_history(self):
+        with open(self._file_name, "rb") as fp:
+            return pickle.load(fp)
 
 
-def add_to_history(folder):
-    history_list = get_history()
-    history_list.append(folder)
+    def add_to_history(self, folder):
+        history_list = self.get_history()
+        history_list.append(folder)
 
-    with open(HISTORY_FILE, "wb") as fp:
-        pickle.dump(history_list, fp)
-
-
-def write_file():
-    with open(HISTORY_FILE, "wb") as fp:
-        pickle.dump([], fp)
+        with open(self._file_name, "wb") as fp:
+            pickle.dump(history_list, fp)
 
 
-def reset():
-    print(f"History :: Reset history cache")
-    write_file()
+    def write_file(self):
+        with open(self._file_name, "wb") as fp:
+            pickle.dump([], fp)
 
 
-if not os.path.isfile(HISTORY_FILE):
-    write_file()
+    def reset(self):
+        print(f"History :: Reset history cache : {self._file_name}")
+        self.write_file()
+
